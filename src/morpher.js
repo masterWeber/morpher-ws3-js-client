7fa60cbf1066a74d5ad8de8bb3c12b205b58a2d3
masterWeber <master.weber@outlook.com>
2021-02-07 12:09:31 +0300

    Added Morpher facade

---
+++ src/morpher.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const Communicator = require('./communicator');
+const RussianClient = require('./russian/client');
+const QazaqClient = require('./qazaq/client');
+
+class Morpher {
+
+    static FLAG_FEMININE = 'feminine';
+    static FLAG_MASCULINE = 'masculine';
+    static FLAG_ANIMATE = 'animate';
+    static FLAG_INANIMATE = 'inanimate';
+    static FLAG_COMMON = 'common';
+    static FLAG_NAME = 'name';
+    static FLAG_NEUTER = 'neuter';
+    static FLAG_PLURAL = 'plural';
+
+    constructor(params) {
+        this.communicator = new Communicator(params);
+    }
+
+    set communicator(value) {
+        this._communicator = value;
+    }
+
+    get communicator() {
+        return this._communicator;
+    }
+
+    get russian() {
+        return new RussianClient(this.communicator);
+    }
+
+    get qazaq() {
+        return new QazaqClient(this.communicator);
+    }
+
+}
+
+module.exports = Morpher;
---
+++ test/morpher.test.js
@@ -0,0 +1,62 @@
+'use strict';
+
+const assert = require('chai').assert;
+const Morpher = require('../src/morpher');
+const RussianClient = require('../src/russian/client');
+const QazaqClient = require('../src/qazaq/client');
+
+describe('Morpher', function () {
+
+    describe('Setting up the Communicator', function () {
+
+        const params = {
+            baseUrl: 'localhost',
+            token: 'TOKEN',
+            timeoutMs: 1000,
+        };
+
+        const morpher = new Morpher(params);
+
+        it("baseUrl is transferred to the communicator", function () {
+            return assert.equal(morpher.communicator.baseUrl, 'http://' + params.baseUrl);
+        });
+
+        it("token is transferred to the communicator", function () {
+            return assert.equal(morpher.communicator.token, params.token);
+        });
+
+        it("timeoutMs is transferred to the communicator", function () {
+            return assert.equal(morpher.communicator.timeoutMs, params.timeoutMs);
+        });
+
+    });
+
+    describe('Communicator replacement', function () {
+
+        class MockCommunicator {
+        }
+
+        const morpher = new Morpher();
+        morpher.communicator = new MockCommunicator();
+
+        it("communicator instance of MockCommunicator", function () {
+            return assert.instanceOf(morpher.communicator, MockCommunicator);
+        });
+
+    });
+
+    describe('Facade', function () {
+
+        const morpher = new Morpher();
+
+        it("field 'russian' instance of RussianClient", function () {
+            return assert.instanceOf(morpher.russian, RussianClient);
+        });
+
+        it("field 'qazaq' instance of QazaqClient", function () {
+            return assert.instanceOf(morpher.qazaq, QazaqClient);
+        });
+
+    });
+
+})
