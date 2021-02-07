ec926f0fbf39e08e5e86517982ed77ef6a53b314
masterWeber <master.weber@outlook.com>
2021-02-07 12:03:15 +0300

    Added MorpherError

---
+++ src/morpher-error.js
@@ -0,0 +1,10 @@
+'use strict';
+
+class MorpherError extends Error {
+    constructor(message, code) {
+        super(message);
+        this.code = code;
+    }
+}
+
+module.exports = MorpherError;
