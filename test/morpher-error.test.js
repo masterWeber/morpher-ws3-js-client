69970a7029761c311c2301f7aa2358cc4c23ce0a
masterWeber <master.weber@outlook.com>
2021-02-07 12:09:53 +0300

    Added MorpherError test

---
+++ test/morpher-error.test.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const assert = require('chai').assert;
+const MorpherError = require('../src/morpher-error');
+
+describe('MorpherError', function () {
+
+    const error = {
+        code: 0,
+        message: 'error message',
+    };
+
+    const morpherError = new MorpherError(error.message, error.code);
+
+    it("set message", function () {
+        return assert.equal(morpherError.message, error.message);
+    });
+
+    it("set code", function () {
+        return assert.equal(morpherError.code, error.code);
+    });
+    
+});
